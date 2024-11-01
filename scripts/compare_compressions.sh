cf666499982520efa761d952032133034d12b5fc
Alexander Anderson <AlexanderAndersonProgramming@gmail.com>
2024-11-01 10:38:27 -0500

    Fix things found while prepping for video

--- scripts/compare_compressions.sh
+++ scripts/compare_compressions.sh
@@ -10,68 +10,70 @@ else
   directory_name=$1
 fi
 
+filename=$(basename $directory_name)
+
 echo "-- ZIP"
 if $verbose_output
   then
-  time zip -r $directory_name.zip $directory_name
+  time zip -r $filename.zip $directory_name
 else
-  time zip -qr $directory_name.zip $directory_name
+  time zip -qr $filename.zip $directory_name
 fi
 
-du -bh $directory_name.zip
+du -h $filename.zip
 echo ""
 
 echo "-- RAR"
 if $verbose_output
   then
-  time rar a -r $directory_name.rar $directory_name
+  time rar a -r $filename.rar $directory_name
 else
-  time rar a -r -inul $directory_name.rar $directory_name
+  time rar a -r -inul $filename.rar $directory_name
 fi
 
-du -bh $directory_name.rar
+du -h $filename.rar
 echo ""
 
 echo "-- GZ"
 if $verbose_output
   then
-  time tar -zcvf $directory_name.tar.gz $directory_name
+  time tar -zcvf $filename.tar.gz $directory_name
 else
-  time tar -zcf $directory_name.tar.gz $directory_name
+  time tar -zcf $filename.tar.gz $directory_name
 fi
 
-du -bh $directory_name.tar.gz
+du -h $filename.tar.gz
 echo ""
 
 echo "-- XZ"
 if $verbose_output
   then
-  time tar -cvf $directory_name.tar.xz $directory_name
+  time tar -cvf $filename.tar.xz $directory_name
 else
-  time tar -cf $directory_name.tar.xz $directory_name
+  time tar -cf $filename.tar.xz $directory_name
 fi
 
-du -bh $directory_name.tar.xz
+du -h $filename.tar.xz
 echo ""
 
 echo "-- 7ZIP"
 if $verbose_output
   then
-  time 7za a -bsp0 $directory_name.7z $directory_name
+  time 7za a -bsp0 $filename.7z $directory_name
 else
-  time 7za a -bso0 -bsp0 $directory_name.7z $directory_name
+  time 7za a -bso0 -bsp0 $filename.7z $directory_name
 fi
 
-du -bh $directory_name.7z
+du -h $filename.7z
 echo ""
 
 echo "-- 7ZIP mx=9"
 if $verbose_output
   then
-  time 7za a -bsp0 -mx=9 $directory_name.7z $directory_name
+  time 7za a -bsp0 -mx=9 $filename.7z $directory_name
 else
-  time 7za a -bso0 -bsp0 -mx=9 $directory_name.7z $directory_name
+  time 7za a -bso0 -bsp0 -mx=9 $filename.7z $directory_name
 fi
 
-du -bh $directory_name.7z
+du -h $filename.7z
 echo ""
